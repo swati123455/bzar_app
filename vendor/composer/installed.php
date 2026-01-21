e7a6b0309a1a4d9acd22198e96e3f27743a3aaf9
swati <48652139+swati123455@users.noreply.github.com>
2026-01-21 13:25:38 +0530

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'l-abdulla/bzar',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ff71b914a2e40bbc015e83b410a26f0614d20441',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'l-abdulla/bzar' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ff71b914a2e40bbc015e83b410a26f0614d20441',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
